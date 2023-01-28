7f3f411e3cdb8e6406be333ec54f89e85bca0123
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2023-01-28 18:05:09 +0530

    Add files via upload

---
+++ A5 Q4.py
@@ -0,0 +1,15 @@
+i = 0
+for i in range(4):
+    j = 0
+    while j<=i:
+        print("* ",end ="")
+        j = j+1
+    print("\n")
+i = 4
+while i>=0:
+    j = 0
+    while j<=i:
+        print("* ",end ="")
+        j = j+1
+    print("\n")
+    i = i-1
